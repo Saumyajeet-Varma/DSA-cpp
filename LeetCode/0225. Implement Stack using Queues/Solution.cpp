8888bd5c54c8541d4b236be2cc17c2dc2383f8e0
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2024-09-01 16:14:14 +0530

    Add files via upload

---
+++ LeetCode/0225. Implement Stack using Queues/Solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack
+{
+public:
+    queue<int> main;
+    queue<int> temp;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        while (!main.empty())
+        {
+            temp.push(main.front());
+            main.pop();
+        }
+        main.push(x);
+        while (!temp.empty())
+        {
+            main.push(temp.front());
+            temp.pop();
+        }
+    }
+
+    int pop()
+    {
+        int el = main.front();
+        main.pop();
+        return el;
+    }
+
+    int top()
+    {
+        return main.front();
+    }
+
+    bool empty()
+    {
+        return main.empty();
+    }
+};
