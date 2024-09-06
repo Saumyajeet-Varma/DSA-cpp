d4cc6300b663b4c351205c84b306a1d9a543ff43
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2024-09-06 21:58:02 +0530

    Add files via upload

---
+++ LeetCode/3217. Delete Nodes From Linked List Present in Array/Solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *modifiedList(vector<int> &nums, ListNode *head)
+    {
+        set<int> st(nums.begin(), nums.end());
+        ListNode *temp = head;
+        while (temp != NULL)
+        {
+            if (st.count(temp->val))
+            {
+                temp = temp->next;
+            }
+            else
+                break;
+        }
+        ListNode *newHead = temp;
+        while (temp != NULL && temp->next != nullptr)
+        {
+            if (st.count(temp->next->val))
+                temp->next = temp->next->next;
+            else
+                temp = temp->next;
+        }
+        return newHead;
+    }
+};
