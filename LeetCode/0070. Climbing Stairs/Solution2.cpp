ebfe5d533de31d7f107cc7e7b36ae3cb47fc91fb
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2024-09-30 22:04:03 +0530

    Add files via upload

---
+++ LeetCode/0070. Climbing Stairs/Solution2.cpp
@@ -0,0 +1,11 @@
+// Time Limit Exceed
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1 || n == 2)
+            return n;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
