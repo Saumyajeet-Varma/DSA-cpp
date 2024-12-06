5d2affe057fd07c92f9f24aee6b35c4a2a356269
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2024-12-06 11:36:51 +0530

    Add files via upload

---
+++ LeetCode/2554. Maximum Number of Integers to Choose From a Range I/Solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxCount(vector<int> &banned, int n, int maxSum)
+    {
+        unordered_set<int> bannedSet(banned.begin(), banned.end());
+        long long sum = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (bannedSet.find(i) != bannedSet.end())
+                continue;
+            sum += i;
+            if (sum > maxSum)
+                break;
+            count++;
+        }
+        return count;
+    }
+};
