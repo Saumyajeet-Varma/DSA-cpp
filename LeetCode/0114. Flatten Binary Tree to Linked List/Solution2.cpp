bac3aef7af6370ff0f9a6e4756485fc252825782
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2024-12-09 16:44:45 +0530

    Add files via upload

---
+++ LeetCode/0114. Flatten Binary Tree to Linked List/Solution2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    void flatten(TreeNode *root)
+    {
+        if (!root)
+            return;
+        stack<TreeNode *> stk;
+        stk.push(root);
+        while (!stk.empty())
+        {
+            TreeNode *node = stk.top();
+            stk.pop();
+            if (node->right)
+                stk.push(node->right);
+            if (node->left)
+                stk.push(node->left);
+            if (!stk.empty())
+                node->right = stk.top();
+            node->left = nullptr;
+        }
+    }
+};
