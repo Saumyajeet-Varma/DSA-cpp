815f3eb836561a34017258727a4fa725e2bdc665
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2025-05-05 17:09:06 +0530

    Add files via upload

---
+++ LeetCode/0790. Domino and Tromino Tiling/Solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    const long MOD = 1e9 + 7;
+
+public:
+    int numTilings(int n)
+    {
+        if (n <= 1)
+            return 1;
+
+        if (n == 2)
+            return 2;
+
+        if (n == 3)
+            return 5;
+
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for (int i = 4; i <= n; i++)
+        {
+            dp[i] = (dp[i - 1] * 2 + long(dp[i - 3])) % MOD;
+        }
+
+        return dp[n];
+    }
+};
