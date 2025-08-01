842fdc95c9dce7b290ac9eb4ab76993fc05fdcd4
Saumyajeet Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2025-08-01 23:20:04 +0530

    Add files via upload

---
+++ LeetCode/0168. Excel Sheet Column Title/Solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string convertToTitle(int columnNumber)
+    {
+        string result = "";
+
+        while (columnNumber)
+        {
+            columnNumber--;
+
+            char ch = 'A' + columnNumber % 26;
+            result = ch + result;
+
+            columnNumber /= 26;
+        }
+
+        return result;
+    }
+};
